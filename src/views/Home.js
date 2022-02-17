4d85cd26a635c8f52750d394adbdc13cc65601d9
Syed Zaid <syed.zaid525@gmail.com>
2022-02-17 23:57:25 +0530

    minor progress in home screen

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import { Header } from '../components';
+import React from "react";
+// import { Header } from '../components';
+import "./css/Home.css"
 
 const Home = () => {
-    return (
-        <div>
-            <Header />
-            <h1>Test</h1>
+  return (
+    <div>
+      {/* <Header /> */}
+      <div className="home" id="home">
+        <div className="content">
+          <div className="name-tag">
+            <h1 className="name">Mohammed Marwan</h1>
+          </div>
+          <div className="bio-tag">
+            <h5 className="bio">One / Two / Poet</h5>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
