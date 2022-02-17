5ad6ef48643bbb80f3309a19577223d57fc082ec
Syed Zaid <syed.zaid525@gmail.com>
2022-02-17 22:56:21 +0530

    initial stage

---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '../components';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Test</h1>
+        </div>
+    );
+};
+
+export default Home;
