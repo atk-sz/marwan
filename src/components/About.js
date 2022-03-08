35d3d132b150a8810b6f0bc4e154afbaea703c80
Syed Zaid <syed.zaid525@gmail.com>
2022-03-08 19:38:39 +0530

    bug fix

---
+++ src/components/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+// import logo from './logo.jpeg';
+import GuiterImage from "../assests/Guiter.jpg";
+import "./css/About.css";
+
+const About = () => {
+  return (
+    <div id="about">
+      {/* <div className="card colour">
+        <div className="row no-gutters">
+          <div className="col-md-6 col-lg-8">
+            <div className="card-body">
+              <h4 className="card-text">
+                <p> Born and brought up in Bangalore</p>
+                <p>22 years old</p>
+                <p>
+                  <em>
+                    F<span>.</span>R<span>.</span>I<span>.</span>E<span>.</span>
+                    N<span>.</span>D<span>.</span>S{" "}
+                  </em>{" "}
+                  freak
+                </p>
+              </h4>
+            </div>
+            <p id="caption">" Psychology is the key to change the world "</p>
+          </div>
+          <div className="card col-md-6 col-lg-4">
+            <img className="card-img" src={GuiterImage} alt="MyPic" />
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default About;
