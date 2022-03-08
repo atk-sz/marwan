2fbf921aca4568a96fb30831154baaef74240d58
Syed Zaid <syed.zaid525@gmail.com>
2022-03-08 20:40:38 +0530

    steady update for about

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,47 @@
 import React from "react";
 // import logo from './logo.jpeg';
-import GuiterImage from "../assests/Guiter.jpg";
+import MyPic from "../assests/hokkah.jpg";
 import "./css/About.css";
 
 const About = () => {
   return (
     <div id="about">
-      {/* <div className="card colour">
-        <div className="row no-gutters">
-          <div className="col-md-6 col-lg-8">
-            <div className="card-body">
-              <h4 className="card-text">
-                <p> Born and brought up in Bangalore</p>
-                <p>22 years old</p>
-                <p>
-                  <em>
-                    F<span>.</span>R<span>.</span>I<span>.</span>E<span>.</span>
-                    N<span>.</span>D<span>.</span>S{" "}
-                  </em>{" "}
-                  freak
-                </p>
-              </h4>
-            </div>
-            <p id="caption">" Psychology is the key to change the world "</p>
-          </div>
-          <div className="card col-md-6 col-lg-4">
-            <img className="card-img" src={GuiterImage} alt="MyPic" />
-          </div>
+      <div className="about-wrap">
+        <div className="about-left">
+          <h4>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, non
+            earum recusandae error voluptate voluptatibus alias quas voluptatum
+            id, tempora ratione nam magni vero repellendus dolores nihil
+            provident. Ex nesciunt vel odit rem, ad asperiores aliquid obcaecati
+            ipsam totam adipisci! Nihil nemo culpa deserunt fugiat ad, quam,
+            reiciendis perferendis laborum aspernatur, repellat ea harum ex!
+          </h4>
         </div>
+        <div className="about-mid">
+          <img className="card-img" src={MyPic} alt="MyPic" />
+        </div>
+      </div>
+      {/* <div
+        className="about-right"
+        style={{ maxWidth: "33.33%", backgroundColor: "black" }}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet libero
+        distinctio quas, praesentium ullam, architecto, voluptate blanditiis
+        magnam provident sequi veniam aut neque ducimus nostrum perferendis
+        recusandae. Dolores, aut nulla.
+      </div> */}
+      {/* <div className="about-left col-md-6 col-lg-8">
+        <h4>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+          accusamus eaque dolorem quae impedit vel earum quia amet commodi,
+          maiores natus, quo est quos! Eligendi dolor fugiat perferendis tempora
+          consectetur architecto. In officiis nobis, beatae modi harum nam
+          consequatur minus quia fugit repellendus odio a. Necessitatibus
+          dolorum fugit doloribus tenetur?
+        </h4>
+      </div>
+      <div className="about-right card col-md-6 col-lg-4">
+        <img className="card-img" src={MyPic} alt="MyPic" />
       </div> */}
     </div>
   );
