4d5cadab8c13043ff668a3c2556f3221f8aa24eb
Syed Zaid <syed.zaid525@gmail.com>
2022-03-08 17:07:26 +0530

    done some imp changes

---
+++ src/components/Landing.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./css/Landing.css"
+
+const Landing = () => {
+  return (
+    <div className="home" id="home">
+      <div className="content">
+        <div className="name-tag">
+          <h1 className="name">Mohammed Marwan</h1>
+        </div>
+        <div className="bio-tag">
+          <h5 className="bio">One / Two / Poet</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,22 +1,13 @@
 import React from "react";
-import { Navbar } from "../components";
+import { Landing, Navbar } from "../components";
 // import { Header } from '../components';
-import "./css/Home.css"
+import "./css/Home.css";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className="home" id="home">
-        <div className="content">
-          <div className="name-tag">
-            <h1 className="name">Mohammed Marwan</h1>
-          </div>
-          <div className="bio-tag">
-            <h5 className="bio">One / Two / Poet</h5>
-          </div>
-        </div>
-      </div>
+      <Landing />
     </div>
   );
 };
